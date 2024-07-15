a43061934079e67e0cef7aed0c11b48310c1b83f
Ramiro Martinez <ramiro.mpdev@gmail.com>
2024-07-15 16:48:02 -0300

    arreglo about nuevo

--- app/(subpages)/about/page.jsx
+++ app/(subpages)/about/page.jsx
@@ -16,7 +16,7 @@ const AboutPage = () => {
             their products.
         </div>
         <div className='about__founder'>
-            <div className='about__founder-container'>
+            <div className='about__founder-container first__right'>
                 <Image
                     className='about__ornament'
                     src="/ornament.png"
@@ -91,6 +91,83 @@ const AboutPage = () => {
                             PhD Degree Bio Composites Engineering
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div className='about__founder second'>
+            <div className='about__founder-container second__right'>
+                <Image
+                    className='about__ornament second__ornament'
+                    src="/ornament.png"
+                    alt="ornament"
+                    width={166}
+                    height={224}
+                    
+                />
+                <Image
+                    className="about__founder-img"
+                    src="/MartinButto.png"
+                    alt="Franco Armanasco"
+                    width={170}
+                    height={170}
+                    priority
+                />
+            </div>
+            <div className='about__founder-info second__left'>
+                <div className='about__founder-subtitle'>Mechanical Engineer</div>
+                <div className='about__founder-name'>Martin Butto</div>
+                <div className='about__founder-text'>
+                Martin was born in Argentina. He graduated with honours from Universidad de Buenos Aires obtaining 
+                a degree in Mechanical Engineering. He began his career working in Tenaris, one of the world’s 
+                largest steel pipes manufacturers, as a process engineer on the heat treatment lines and as a 
+                product engineer on both sucker rods division and on pipes for automotive, mining, energy and 
+                general-purpose applications. Given his draw towards material science and recognizing the 
+                ever-growing applications of plastics, he decided to pursue a PhD in engineering in 2020. 
+                His research was aimed towards the development of self reinforced, biobased and biodegradable 
+                thermoplastics. He then decided to go back into an industrial setting to gain hands-on experience 
+                on composite design and manufacturing. So far, he has been working on the application of composite 
+                materials to high-performance agricultural applications.
+                </div>
+                <div className='about__founder-degrees'>
+                    <div className='about__founder-degreesItem second__degreesItem'>
+                        <Image
+                            src="/degree1.svg"
+                            alt="University Degree
+                            Marine Engineering"
+                            width={47}
+                            height={47}
+                            
+                        />
+                        <div>
+                        University Degree Mechanical Engineering
+                        </div>
+                    </div>
+                    {/* <div className='about__founder-degreesItem'>
+                        <Image
+                            src="/degree2.svg"
+                            alt="Master’s Degree
+                            Composites Engineering"
+                            width={47}
+                            height={47}
+                            
+                        />
+                        <div>
+                        Master’s Degree Composites Engineering
+                        </div>
+                    </div>
+                    <div className='about__founder-degreesItem'>
+                        <Image
+                            src="/title1.svg"
+                            alt="Doctor of Philosophy
+                            Bio Composites"
+                            width={47}
+                            height={47}
+                            
+                        />
+                        <div>
+                            PhD Degree Bio Composites Engineering
+                        </div>
+                    </div> */}
                 </div>
             </div>
         </div>
