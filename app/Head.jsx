548e523acb6a8975e30cc40a4071d5491c4fc825
Ramiro Martinez Prieto <ext_raprieto@mercadolibre.com>
2025-09-17 10:57:23 -0300

    arreglos

--- app/Head.jsx
+++ app/Head.jsx
@@ -1,31 +1,41 @@
 "use client";
-import React from 'react'
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
 import CookieModal from './components/CookieModal';
 
 const Head = () => {
+  const [cookieAccepted, setCookieAccepted] = useState(null);
 
   useEffect(() => {
-    ReactGA.initialize('TU_ID_DE_SEGUIMIENTO', {
-      gaOptions: {
-        cookieFlags: 'samesite=none;secure',
-      },
-    }); // Reemplaza 'TU_ID_DE_SEGUIMIENTO' con el ID de seguimiento real
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    const storedConsent = localStorage.getItem("cookieAccepted");
+    if (storedConsent === "true") {
+      setCookieAccepted(true);
+      ReactGA.initialize("TU_ID_DE_SEGUIMIENTO", {
+        gaOptions: { cookieFlags: "samesite=none;secure" },
+      });
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    } else {
+      setCookieAccepted(false);
+    }
   }, []);
 
-  const [cookieAccepted, setCookieAccepted] = React.useState(false);
-
   const handleAcceptCookie = () => {
+    localStorage.setItem("cookieAccepted", "true");
     setCookieAccepted(true);
+
+    ReactGA.initialize("TU_ID_DE_SEGUIMIENTO", {
+      gaOptions: { cookieFlags: "samesite=none;secure" },
+    });
+    ReactGA.pageview(window.location.pathname + window.location.search);
   };
+
+  if (cookieAccepted === null) return null;
 
   return (
     <>
       {!cookieAccepted && <CookieModal onAccept={handleAcceptCookie} />}
     </>
   );
-}
+};
 
-export default Head
+export default Head;
